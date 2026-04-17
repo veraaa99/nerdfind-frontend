fe8a4a90bbd2226c55bb9316d039a5f9c618733b
veraaa99 <althini.vera@gmail.com>
2026-04-17 10:19:54 +0200

    Started working on the home page structure

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,18 @@
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <div>
+        <h1 className="text-center">NERDFIND SVERIGE</h1>
+        <h2 className="text-center">
+          SVERIGES GEMENSAMMA PORTAL FÖR NÖRDEVENT, LOPPISAR, MÄSSOR OCH
+          BUTIKER!
+        </h2>
+      </div>
+      <div>
+        <h3>Utforska våra populära annonser nedan!</h3>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
